bb9673c6a97c946ba421c4a145802378889a465d
SHIVANI MANDHANIA <shivanimandhania@SHIVANIs-MacBook-Air.local>
2026-01-30 23:55:38 +0530

    Progress

---
+++ BASICS/script.js
@@ -0,0 +1,19 @@
+console.log("separation of concern");
+
+let a="tinummm";
+a=10;
+console.log(a);
+let name="mine";
+
+let tina={
+
+    name:'tinu',
+    height:170,
+    feature:'most capable person on planet',
+    characteristic:'extremely intelligent',
+      work: function()
+      {
+        console.log("Earning 3l/month from work");
+      }
+
+};
